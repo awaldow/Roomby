96387d41c2c7e56e9e49a0a5a7631cd4bd22a6a3
Addison Waldow <addison.waldow@phoenix.gov>
2021-01-25 09:48:06 -0700

    Add no-op unit test projects

---
+++ Roomby.API.Rooms.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Roomby.API.Rooms.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Roomby.API.Users.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Roomby.API.Users.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
