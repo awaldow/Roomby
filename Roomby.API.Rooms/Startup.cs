db8cd4a59b3127f9fbe5d752689b6a492c5ca407
Addison Waldow <addison.waldow@phoenix.gov>
2021-01-27 13:23:54 -0700

    make swagger available outside of dev

--- Roomby.API.Rooms/Startup.cs
+++ Roomby.API.Rooms/Startup.cs
@@ -40,9 +40,10 @@ namespace Roomby.API.Rooms
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Roomby.API v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Roomby.API v1"));
 
             app.UseHttpsRedirection();
 
