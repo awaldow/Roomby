29a55c72e12794a7d120f06569290119e7950e62
Addison Waldow <addison.waldow@phoenix.gov>
2021-01-30 14:17:39 -0700

    start adding some xml comments

--- Roomby.API.Rooms/Controllers/RoomController.cs
+++ Roomby.API.Rooms/Controllers/RoomController.cs
@@ -33,6 +33,12 @@ namespace Roomby.API.Rooms.Controllers
 
 
         #region V1
+        /// <summary>GetRoomsForHouseholdAsync(householdId)</summary>
+        /// <remarks>
+        /// Returns a list of Rooms (sorted by name) for a given Household ID
+        /// </remarks>
+        /// <param name="householdId">The Household ID to return a list of Rooms for</param>
+        /// <returns>A List of Room objects for the given Household ID</returns>
         [HttpGet("{householdId}", Name = "GetRoomsForHouseholdAsync")]
         [MapToApiVersion("1")]
         [ProducesResponseType(typeof(List<Room>), StatusCodes.Status200OK)]
@@ -51,12 +57,20 @@ namespace Roomby.API.Rooms.Controllers
             }
         }
 
+        /// <summary>
+        /// GetRoomAsync(roomId)
+        /// </summary>
+        /// <remarks>
+        /// Returns the Room object for <paramref name="roomId"/>
+        /// </remarks>
+        /// <param name="roomId">Room ID for the Room to get</param>
+        /// <returns>Room object with id <paramref name="roomId"/></returns>
         [HttpGet("{roomId}", Name = "GetRoom")]
         [MapToApiVersion("1")]
         [ProducesResponseType(typeof(Room), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<Room>> GetRoom(Guid roomId)
+        public async Task<ActionResult<Room>> GetRoomAsync(Guid roomId)
         {
             try
             {
@@ -77,26 +91,37 @@ namespace Roomby.API.Rooms.Controllers
             }
         }
 
-        [HttpPost(Name = "CreateRoom")]
+        /// <summary>
+        /// CreateRoomAsync(roomToCreate)
+        /// </summary>
+        /// <remarks>
+        /// Creates the provided <paramref name="roomToCreate"/>
+        /// </remarks>
+        /// <param name="roomToCreate">A Room object. See <see cref="Roomby.API.Rooms.Mediators.CreateRoomValidator"/> for validation information</param>
+        /// <returns>The created Room object</returns>
+        [HttpPost(Name = "CreateRoomAsync")]
         [MapToApiVersion("1")]
+        [Consumes("application/json")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(Room), StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<Room>> CreateRoom(Room roomToCreate)
+        
+        public async Task<ActionResult<Room>> CreateRoomAsync(Room roomToCreate)
         {
             // TODO: use bad requests here to test global 500 filter, maybe we don't need those try catches in the other controller actions
             var created = await _mediator.Send(new CreateRoom { RoomToCreate = roomToCreate });
 
-            return CreatedAtAction(nameof(this.GetRoom), new { id = created.Id }, created);
+            return CreatedAtAction(nameof(this.GetRoomAsync), new { id = created.Id }, created);
         }
 
         [HttpPut("{roomId}", Name = "UpdateRoom")]
         [MapToApiVersion("1")]
+        [Consumes("application/json")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(Room), StatusCodes.Status201Created)]
         [ProducesResponseType(typeof(Room), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<Room>> UpdateRoom(Guid roomId, [FromBody] Room roomToUpdate)
+        public async Task<ActionResult<Room>> UpdateRoomAsync(Guid roomId, [FromBody] Room roomToUpdate)
         {
             try
             {
@@ -107,7 +132,7 @@ namespace Roomby.API.Rooms.Controllers
                 }
                 else if (created != null)
                 {
-                    return CreatedAtAction(nameof(this.GetRoom), new { id = created.Id }, created);
+                    return CreatedAtAction(nameof(this.GetRoomAsync), new { id = created.Id }, created);
                 }
                 else
                 {
@@ -132,7 +157,7 @@ namespace Roomby.API.Rooms.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult> DeleteRoom(Guid roomId)
+        public async Task<ActionResult> DeleteRoomAsync(Guid roomId)
         {
             (bool roomWasDeleted, Room deletedRoom) = await _mediator.Send(new DeleteRoom { RoomId = roomId });
             if (roomWasDeleted)
