4beb8acb06029afe0f25c5060b3d7bb49d76ef4a
Addison Waldow <a.wal.bear@gmail.com>
2021-02-04 19:40:35 +0000

    Updated genSwagger.ps1

--- Roomby.API.Users/genSwagger.ps1
+++ Roomby.API.Users/genSwagger.ps1
@@ -1,3 +1,3 @@
-dotnet tool run swagger tofile --output Roomby.API.Users.v1.json bin/Debug/net5.0/Roomby.API.Users.dll v1
+dotnet tool run swagger tofile --output Roomby.API.Users.v1.json bin/Release/net5.0/Roomby.API.Users.dll v1
 
 dotnet tool run openapi-to-terraform -f Roomby.API.Users.v1.json -o ../terraform/test/users -t terraformVarSub.json
