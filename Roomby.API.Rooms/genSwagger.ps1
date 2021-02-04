a0f954d3ddf239b1984a446b70a16c5f5e0224b1
Addison Waldow <addison.waldow@phoenix.gov>
2021-02-04 14:30:28 -0700

    fix rooms genswagger

--- Roomby.API.Rooms/genSwagger.ps1
+++ Roomby.API.Rooms/genSwagger.ps1
@@ -1,3 +1,3 @@
 dotnet tool run swagger tofile --output Roomby.API.Rooms.v1.json bin/$BUILD_CONFIGURATION/net5.0/Roomby.API.Rooms.dll v1
 
-dotnet tool run openapi-to-terraform -f Roomby.API.Rooms.v1.json -o /home/awaldow/source/repos/roomby/roomby.api/terraform/test/rooms -t terraformVarSub.json
+dotnet tool run openapi-to-terraform -f Roomby.API.Rooms.v1.json -o ../terraform/test/rooms -t terraformVarSub.json
