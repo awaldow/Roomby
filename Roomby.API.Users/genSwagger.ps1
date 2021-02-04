ea8a2702e8a1618b5e3ed5a2333c482d297cf321
Addison Waldow <addison.waldow@phoenix.gov>
2021-02-04 10:09:17 -0700

    merge terraform file generation into one action

--- Roomby.API.Users/genSwagger.ps1
+++ Roomby.API.Users/genSwagger.ps1
@@ -1,1 +1,3 @@
 dotnet tool run swagger tofile --output Roomby.API.Users.v1.json bin/Debug/net5.0/Roomby.API.Users.dll v1
+
+dotnet tool run openapi-to-terraform -f Roomby.API.Users.v1.json -o /home/awaldow/source/repos/roomby/roomby.api/terraform/test/users -t terraformVarSub.json
